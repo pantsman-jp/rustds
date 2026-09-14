9dea8bbfa3b312ac438c920eb1af86318965de50
pantsman-jp <214776175+pantsman-jp@users.noreply.github.com>
2026-09-14 20:35:49 +0900

    push method

--- src/stack.rs
+++ src/stack.rs
@@ -6,6 +6,10 @@ impl<T> Stack<T> {
     pub fn new() -> Self {
         Stack { data: Vec::new() }
     }
+
+    pub fn push(&mut self, value: T) {
+        self.data.push(value);
+    }
 }
 
 #[cfg(test)]
@@ -16,5 +20,14 @@ mod tests {
     fn new_creates_empty_stack() {
         let stack = Stack::<i32>::new();
         assert!(stack.data.is_empty());
+    }
+
+    #[test]
+    fn push_adds_element() {
+        let mut stack = Stack::<i32>::new();
+        stack.push(0);
+        assert_eq!(stack.data, vec![0]);
+        stack.push(1);
+        assert_eq!(stack.data, vec![0, 1]);
     }
 }
