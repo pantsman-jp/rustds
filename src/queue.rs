f10e5b354bbe70577e34a1f805eae785767f3fa0
pantsman-jp <214776175+pantsman-jp@users.noreply.github.com>
2026-09-15 10:51:20 +0900

    pop method

--- src/queue.rs
+++ src/queue.rs
@@ -15,6 +15,10 @@ impl<T> Queue<T> {
     pub fn push(&mut self, value: T) {
         self.data.push_back(value);
     }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.data.pop_front()
+    }
 }
 
 #[cfg(test)]
@@ -34,5 +38,13 @@ mod tests {
         assert_eq!(q.data, [10]);
         q.push(20);
         assert_eq!(q.data, [10, 20]);
+    }
+
+    #[test]
+    fn pop_element() {
+        let mut q = Queue::<i32>::new();
+        q.push(10);
+        assert_eq!(q.data, [10]);
+        assert_eq!(q.pop(), Some(10));
     }
 }
