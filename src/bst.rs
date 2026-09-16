fed1dd5fb6520fc14e69e8b779e1860d0afe0c23
pantsman-jp <214776175+pantsman-jp@users.noreply.github.com>
2026-09-16 14:26:54 +0900

    insert method

--- src/bst.rs
+++ src/bst.rs
@@ -16,6 +16,29 @@ impl<T: Ord> BST<T> {
     pub fn is_empty(&self) -> bool {
         self.tree.is_none()
     }
+
+    fn insert_node(node: &mut Option<Box<Node<T>>>, value: T) {
+        match node {
+            Some(tree) => {
+                if value < tree.value {
+                    Self::insert_node(&mut tree.left, value);
+                } else if value > tree.value {
+                    Self::insert_node(&mut tree.right, value);
+                }
+            }
+            None => {
+                *node = Some(Box::new(Node {
+                    left: None,
+                    value: value,
+                    right: None,
+                }));
+            }
+        }
+    }
+
+    pub fn insert(&mut self, value: T) {
+        Self::insert_node(&mut self.tree, value);
+    }
 }
 
 #[cfg(test)]
@@ -26,5 +49,13 @@ mod tests {
     fn test_new() {
         let bst = BST::<i32>::new();
         assert!(bst.is_empty());
+    }
+
+    #[test]
+    fn test_insert() {
+        let mut bst = BST::<i32>::new();
+        assert!(bst.is_empty());
+        bst.insert(8);
+        assert!(!bst.is_empty());
     }
 }
