71293f098302ca87e76562ab9faae87b98e51a2e
pantsman-jp <214776175+pantsman-jp@users.noreply.github.com>
2026-09-17 13:15:16 +0900

    push method

--- src/heap.rs
+++ src/heap.rs
@@ -10,6 +10,31 @@ impl<T: Ord> Heap<T> {
     pub fn is_empty(&self) -> bool {
         self.data.is_empty()
     }
+
+    fn left_idx(index: &usize) -> usize {
+        2 * index + 1
+    }
+
+    fn right_idx(index: &usize) -> usize {
+        2 * index + 2
+    }
+
+    fn parent_idx(index: &usize) -> usize {
+        (index - 1) / 2
+    }
+
+    pub fn push(&mut self, value: T) {
+        self.data.push(value);
+        let mut index = self.data.len() - 1;
+        while index > 0 {
+            let parent_idx = Self::parent_idx(&index);
+            if self.data[parent_idx] <= self.data[index] {
+                break;
+            }
+            self.data.swap(parent_idx, index);
+            index = parent_idx;
+        }
+    }
 }
 
 #[cfg(test)]
