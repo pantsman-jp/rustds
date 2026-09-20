158ddbc4d3992e56806b3a535e88d545a014e837
pantsman-jp <214776175+pantsman-jp@users.noreply.github.com>
2026-09-20 21:18:47 +0900

    inplement bucket_index

--- src/hash_table.rs
+++ src/hash_table.rs
@@ -1,10 +1,20 @@
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
 pub struct HashTable<K, V> {
     buckets: Vec<Vec<(K, V)>>,
 }
 
-impl<K, V> HashTable<K, V> {
+impl<K: Hash, V> HashTable<K, V> {
     pub fn new() -> Self {
         let buckets = (0..16).map(|_| Vec::new()).collect();
         HashTable { buckets }
+    }
+
+    fn bucket_index(&self, key: &K) -> usize {
+        let mut hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+        let hash = hasher.finish();
+        (hash % self.buckets.len() as u64) as usize
     }
 }
