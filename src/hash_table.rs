a38e491f97a4da4198cfae993293acfc2408d72e
pantsman-jp <214776175+pantsman-jp@users.noreply.github.com>
2026-09-20 21:23:48 +0900

    insert method

--- src/hash_table.rs
+++ src/hash_table.rs
@@ -5,7 +5,7 @@ pub struct HashTable<K, V> {
     buckets: Vec<Vec<(K, V)>>,
 }
 
-impl<K: Hash, V> HashTable<K, V> {
+impl<K: Hash + Eq, V> HashTable<K, V> {
     pub fn new() -> Self {
         let buckets = (0..16).map(|_| Vec::new()).collect();
         HashTable { buckets }
@@ -16,5 +16,16 @@ impl<K: Hash, V> HashTable<K, V> {
         key.hash(&mut hasher);
         let hash = hasher.finish();
         (hash % self.buckets.len() as u64) as usize
+    }
+
+    pub fn insert(&mut self, key: K, value: V) {
+        let index = self.bucket_index(&key);
+        for (stored_key, stored_value) in &mut self.buckets[index] {
+            if stored_key == &key {
+                *stored_value = value;
+                return;
+            }
+        }
+        self.buckets[index].push((key, value));
     }
 }
